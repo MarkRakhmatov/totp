42ffbde87158711fa1148703f6d05ff928f868c0
Mark <mark.rakhmatov@gmail.com>
2026-01-26 00:44:59 +0200

    refactor(#3): fix fuzz tests

--- fuzz_test/src/totp_test.cpp
+++ fuzz_test/src/totp_test.cpp
@@ -6,13 +6,11 @@
 #include <totp/totp.hpp>
 #include <string>
 
-void totpNullptrOrNotEptyString(std::string s, long seconds) {
+void totpEmptyOrValidString(std::string s, long seconds) {
   auto totp = otp::getTOTP(s, seconds);
-  if (totp != nullptr) {
-    auto totpStr = std::string(totp.get());
-    EXPECT_FALSE(totpStr.empty());
-    EXPECT_EQ(totpStr.size(), 6);
+  if (!totp.empty()) {
+    EXPECT_EQ(totp.size(), 6);
   }
 }
 
-FUZZ_TEST(totpFuzzTestSuite, totpNullptrOrNotEptyString);
+FUZZ_TEST(totpFuzzTestSuite, totpEmptyOrValidString);
